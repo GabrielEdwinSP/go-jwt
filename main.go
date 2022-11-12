f129dd388f485f17f69ecd82161b7341565d9e29
GabrielEdwinSP <gabrieledwinsp@gmail.com>
2022-11-12 11:47:34 +0700

    Create JWT

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/GabrielEdwinSP/go-jwt/controllers"
+	"github.com/GabrielEdwinSP/go-jwt/initializers"
+	"github.com/GabrielEdwinSP/go-jwt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	r.Run()
+}
